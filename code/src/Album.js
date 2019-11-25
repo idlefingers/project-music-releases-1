0b0134e684c3f9478e826e931b822462449dee35
Damien <mail@damientimewell.com>
2019-11-25 16:13:06 +0100

    Changes from call

--- code/src/Album.js
+++ code/src/Album.js
@@ -8,41 +8,41 @@ console.log(data.album.items[1])*/
 
 
 export const Album = (props) => {
-    return (
+  return (
 
-        <div>
-            <div className="album-cover">
-                {/* --ta bort img-- */}
-                <img className="album-img" src={props.image} alt="Album" />
+    <div className="album">
+      <div className="album-cover">
+        {/* --ta bort img-- */}
+        <img className="album-img" src={props.image} alt="Album" />
 
-                <div className="icons">
-                    <img className="icon-heart" src="icons/heart.svg" alt="Heart" />
-                    <img className="icon-play" src="icons/play.svg" alt="Play" />
-                    <img className="icon-dots" src="icons/dots.svg" alt="Dots" />
-                </div>
-            </div>
-
-
-            <div className="album-info">
-                <div className="album-title">
-                    <a href={props.albumURL}>{props.albumTitle}</a>
-                </div>
-            </div>
-
-
-
-
-            <div className="album-info">
-                <div className="album-title">
-                    <a href={props.albumURL}>{props.name}</a>
-                </div>
-                <div className="artist-name">
-                    <a href={props.albumURL}>{props.artists}</a>
-
-                </div>
-            </div>
+        <div className="icons">
+          <img className="icon-heart" src="icons/heart.svg" alt="Heart" />
+          <img className="icon-play" src="icons/play.svg" alt="Play" />
+          <img className="icon-dots" src="icons/dots.svg" alt="Dots" />
         </div>
-    )
+      </div>
+
+
+      <div className="album-info">
+        <div className="album-title">
+          <a href={props.albumURL}>{props.albumTitle}</a>
+        </div>
+      </div>
+
+
+
+
+      <div className="album-info">
+        <div className="album-title">
+          <a href={props.albumURL}>{props.name}</a>
+        </div>
+        <div className="artist-name">
+          <a href={props.albumURL}>{props.artists}</a>
+
+        </div>
+      </div>
+    </div>
+  )
 }
 
 
